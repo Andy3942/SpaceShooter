78c25662fcc2b698f5285cd1919a6dbf5ef2ff0f
Andy3942 <aa394215561@gmail.com>
2015-05-26 19:05:39 +0800

    ""

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	void Update()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = movement * speed;
+		if (rigidbody.velocity.x > 0) {
+			rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, -rigidbody.velocity.x * tilt);		
+		} else if (rigidbody.velocity.x < 0) {
+			rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, -rigidbody.velocity.x * tilt);
+		} else 
+		{
+			rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, 0.0f);
+		}
+		rigidbody.position = new Vector3 (Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), rigidbody.position.y, Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+	}
+}
